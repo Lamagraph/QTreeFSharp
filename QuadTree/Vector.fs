6f246bb3af4130749588e0fc46ac0afd179ce782
gsv <rsdpisuy@gmail.com>
2026-02-03 10:24:59 +0300

    First test on Vector.map2.

--- QuadTree/Vector.fs
+++ QuadTree/Vector.fs
@@ -54,28 +54,34 @@ let map2 (vector1: SparseVector<'a>) (vector2: SparseVector<'b>) f =
     let len1 = vector1.length
 
     if len1 = vector2.length then
-        let rec inner (len: uint64<storageSize>) vector1 vector2 =
+        let rec inner (size: uint64<storageSize>) vector1 vector2 =
             match (vector1, vector2) with
             | Node(t1, t2), Leaf(_) ->
-                let new_t1, nvals1 = inner (len / 2UL) t1 vector2
-                let new_t2, nvals2 = inner (len / 2UL) t2 vector2
+                let new_t1, nvals1 = inner (size / 2UL) t1 vector2
+                let new_t2, nvals2 = inner (size / 2UL) t2 vector2
                 (mkNode new_t1 new_t2), nvals1 + nvals2
             | Leaf(_), Node(t1, t2) ->
-                let new_t1, nvals1 = inner (len / 2UL) vector1 t1
-                let new_t2, nvals2 = inner (len / 2UL) vector1 t2
+                let new_t1, nvals1 = inner (size / 2UL) vector1 t1
+                let new_t2, nvals2 = inner (size / 2UL) vector1 t2
                 (mkNode new_t1 new_t2), nvals1 + nvals2
             | Node(t1, t2), Node(t3, t4) ->
-                let new_t1, nvals1 = inner (len / 2UL) t1 t3
-                let new_t2, nvals2 = inner (len / 2UL) t2 t4
+                let new_t1, nvals1 = inner (size / 2UL) t1 t3
+                let new_t2, nvals2 = inner (size / 2UL) t2 t4
                 (mkNode new_t1 new_t2), nvals1 + nvals2
-            | Leaf(v1), Leaf(v2) -> Leaf(f v1 v2), ((uint64 len) * 1UL<nvals>)
+            | Leaf(v1), Leaf(v2) ->
+                let res = f v1 v2
+
+                let nnz =
+                    match res with
+                    | None -> 0UL<nvals>
+                    | _ -> (uint64 size) * 1UL<nvals>
+
+                Leaf(res), nnz
 
         let storage, nvals =
             inner vector1.storage.size vector1.storage.data vector2.storage.data
 
-        Result.Success(
-            SparseVector(len1, nvals, (Storage((getNearestUpperPowerOfTwo (uint64 nvals)) * 1UL<storageSize>, storage)))
-        )
+        Result.Success(SparseVector(len1, nvals, (Storage(vector1.storage.size, storage))))
     else
         Result.Failure "The length of the vector1 must be equals to the length of the vector2."
 
