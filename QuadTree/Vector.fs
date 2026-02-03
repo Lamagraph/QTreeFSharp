c9695cac1e1cacfd1077ff4b7b197225538e5263
gsv <rsdpisuy@gmail.com>
2026-02-03 09:00:33 +0300

    Draft of nearest power of two calculation.

--- QuadTree/Vector.fs
+++ QuadTree/Vector.fs
@@ -1,14 +1,28 @@
 module Vector
 
+open Common
+
 type 'value btree =
     | Leaf of 'value
     | Node of 'value btree * 'value btree
 
+[<Measure>]
+type dataLength
+
+[<Measure>]
+type storageSize
+
+[<Struct>]
+type Storage<'value> =
+    val size: uint64<storageSize>
+    val data: btree<'value>
+    new(_size, _data) = { size = _size; data = _data }
+
 [<Struct>]
 type SparseVector<'value> =
-    val length: uint
-    val nvals: uint
-    val storage: btree<Option<'value>>
+    val length: uint64<dataLength>
+    val nvals: uint64<nvals>
+    val storage: Storage<Option<'value>>
 
     new(_length, _nvals, _storage) =
         { length = _length
@@ -40,24 +54,28 @@ let map2 (vector1: SparseVector<'a>) (vector2: SparseVector<'b>) f =
     let len1 = vector1.length
 
     if len1 = vector2.length then
-        let rec inner (len: uint) vector1 vector2 =
+        let rec inner (len: uint64<storageSize>) vector1 vector2 =
             match (vector1, vector2) with
             | Node(t1, t2), Leaf(_) ->
-                let new_t1, nvals1 = inner (len / 2u) t1 vector2
-                let new_t2, nvals2 = inner (len / 2u) t2 vector2
+                let new_t1, nvals1 = inner (len / 2UL) t1 vector2
+                let new_t2, nvals2 = inner (len / 2UL) t2 vector2
                 (mkNode new_t1 new_t2), nvals1 + nvals2
             | Leaf(_), Node(t1, t2) ->
-                let new_t1, nvals1 = inner (len / 2u) vector1 t1
-                let new_t2, nvals2 = inner (len / 2u) vector1 t2
+                let new_t1, nvals1 = inner (len / 2UL) vector1 t1
+                let new_t2, nvals2 = inner (len / 2UL) vector1 t2
                 (mkNode new_t1 new_t2), nvals1 + nvals2
             | Node(t1, t2), Node(t3, t4) ->
-                let new_t1, nvals1 = inner (len / 2u) t1 t3
-                let new_t2, nvals2 = inner (len / 2u) t2 t4
+                let new_t1, nvals1 = inner (len / 2UL) t1 t3
+                let new_t2, nvals2 = inner (len / 2UL) t2 t4
                 (mkNode new_t1 new_t2), nvals1 + nvals2
-            | Leaf(v1), Leaf(v2) -> Leaf(f v1 v2), len
+            | Leaf(v1), Leaf(v2) -> Leaf(f v1 v2), ((uint64 len) * 1UL<nvals>)
 
-        let storage, nvals = inner len1 vector1.storage vector2.storage
-        Result.Success(SparseVector(len1, nvals, storage))
+        let storage, nvals =
+            inner vector1.storage.size vector1.storage.data vector2.storage.data
+
+        Result.Success(
+            SparseVector(len1, nvals, (Storage((getNearestUpperPowerOfTwo (uint64 nvals)) * 1UL<storageSize>, storage)))
+        )
     else
         Result.Failure "The length of the vector1 must be equals to the length of the vector2."
 
