b977c5ed00ff0e9d9a9016099bae482eb5b415a9
gsv <rsdpisuy@gmail.com>
2026-03-23 19:32:30 +0300

    WIP. Prepare to add Vector.mapi for Boruvka

--- QuadTree/Boruvka.fs
+++ QuadTree/Boruvka.fs
@@ -2,11 +2,6 @@ module Graph.Boruvka
 
 open Common
 
-type Error<'t1, 't2, 't3, 't4> =
-    | NewFrontierCalculationProblem of LinearAlgebra.Error<'t1, 't2, 't1>
-    | EdgesCalculationProblem of LinearAlgebra.Error<'t1, 't2, 't3>
-    | CEdgesCalculationProblem of Vector.Error<'t1, 't4, 't4>
-
 (*
 Вход: граф G = (V, E, w), матрица смежности S, n = |V|
 Выход: множество рёбер МОД T
@@ -81,6 +76,11 @@ while S not empty do {
 return T
 *)
 
+type Error<'t1, 't2, 't3, 't4> =
+    | NewFrontierCalculationProblem of LinearAlgebra.Error<'t1, 't2, 't1>
+    | EdgesCalculationProblem of Vector.Error<'t1, 't2, 't3>
+    | CEdgesCalculationProblem of Vector.Error<'t1, 't4, 't4>
+(*
 let mst (graph:Matrix.SparseMatrix<_>) =
     let op_add x y =
         match (x, y) with
@@ -94,6 +94,7 @@ let mst (graph:Matrix.SparseMatrix<_>) =
     
     let length = uint64 graph.nrows * 1UL<Vector.dataLength>
     let parent = Vector.init  length (fun i -> Some i)
+    //let all_n = Vector.init  length (fun _ ->  Some (uint64 graph.ncols * 1UL<Vector.index>))
     let iota = Vector.init  length (fun i -> Some (uint64 i))
     
     let rec inner (graph: Matrix.SparseMatrix<_>) (tree: Matrix.SparseMatrix<_>) =
@@ -118,11 +119,134 @@ let mst (graph:Matrix.SparseMatrix<_>) =
                 | Result.Success(cedges) ->
 
                     let t = Vector.gather cedges parent
-
-                    inner graph
+                    let mask = Vector.map2 edges t (fun x y -> match (x,y) with Some v1, Some v2 when v1 = v2 -> Some v1 | _ -> None)
+                    match mask with 
+                    | Result.Failure(e) -> Result.Failure(CEdgesCalculationProblem(e))
+                    | Result.Success (mask) ->
+                        let index = Vector.map2 iota mask (fun i m -> match m with | Some _ -> )
+                        inner graph
 
                 
         else
             Result.Success tree
 
     inner graph
+*)
+
+
+let mst (graph:Matrix.SparseMatrix<'w>) =
+    let n = uint64 graph.nrows
+
+    let findRoot (parent: uint64 array) x =
+        let rec loop (y: uint64) (visited: Set<uint64>) =
+            if visited.Contains(y) then y
+            else
+                let p = parent.[int y]
+                if p <> y then loop p (Set.add y visited) else y
+        loop x Set.empty
+
+    let union (parent: uint64 array) (rank: uint64 array) x y =
+        let rootX = findRoot parent x
+        let rootY = findRoot parent y
+        if rootX <> rootY then
+            if rank.[int rootX] < rank.[int rootY] then
+                parent.[int rootX] <- rootY
+            elif rank.[int rootX] > rank.[int rootY] then
+                parent.[int rootY] <- rootX
+            else
+                parent.[int rootY] <- rootX
+                rank.[int rootX] <- rank.[int rootX] + 1UL
+
+    let rec inner (S: Matrix.SparseMatrix<'w>) (mstEdges: (uint64 * uint64 * 'w) list) (parent: uint64 array) (rank: uint64 array) (iterations: int) =
+        if S.nvals > 0UL<nvals> && iterations < 20 then
+            let edgesCL = Matrix.toCoordinateList S
+            
+            let edgesData = 
+                edgesCL.list 
+                |> List.map (fun (i, j, w) -> (uint64 i, uint64 j, w))
+
+            let edgesWithCompInfo =
+                edgesData
+                |> List.map (fun (i, j, w) -> 
+                    let rootJ = findRoot parent j
+                    let rootI = findRoot parent i
+                    (i, j, w, rootI, rootJ))
+
+            let interCompEdges =
+                edgesWithCompInfo
+                |> List.filter (fun (i, j, w, rootI, rootJ) -> rootI <> rootJ)
+
+            if interCompEdges.IsEmpty then
+                let resultCL =
+                    Matrix.CoordinateList(graph.nrows, graph.ncols,
+                        List.map (fun (i, j, w) -> (i * 1UL<Matrix.rowindex>, j * 1UL<Matrix.colindex>, w)) mstEdges)
+                Result.Success (Matrix.fromCoordinateList resultCL)
+            else
+                let compMinEdges =
+                    interCompEdges
+                    |> List.groupBy (fun (_, _, _, _, rootJ) -> rootJ)
+                    |> List.map (fun (rootJ, edges) ->
+                        let minEdge = List.minBy (fun (i, j, w, _, _) -> w) edges
+                        (rootJ, minEdge))
+                    |> Map.ofList
+
+                let repToEdge =
+                    compMinEdges
+                    |> Map.toList
+                    |> List.map (fun (destComp, edge) -> (destComp, edge))
+                    |> List.filter (fun (_, (minI, minJ, _, _, _)) -> 
+                        let canonicalI, canonicalJ = if minI < minJ then minI, minJ else minJ, minI
+                        let srcRoot = findRoot parent canonicalI
+                        let dstRoot = findRoot parent canonicalJ
+                        srcRoot <> dstRoot)
+                    |> List.distinctBy (fun (_, (minI, minJ, _, _, _)) -> 
+                        if minI < minJ then (minI, minJ) else (minJ, minI))
+
+                let newMstEdges =
+                    repToEdge
+                    |> List.collect (fun (_, (minI, minJ, minW, _, _)) -> 
+                        [(minI, minJ, minW); (minJ, minI, minW)])
+                    |> fun newEdges -> mstEdges @ newEdges
+
+                let parentCopy = Array.copy parent
+                let rankCopy = Array.copy rank
+
+                for (_, (minI, minJ, _, _, _)) in repToEdge do
+                    union parentCopy rankCopy minI minJ
+
+                let rec pointerJump (parentArr: uint64 array) maxIter =
+                    if maxIter = 0 then parentArr
+                    else
+                        let newParent = Array.copy parentArr
+                        let mutable changed = false
+                        for i in 0..int n-1 do
+                            let root = findRoot parentArr parentArr.[i]
+                            if root <> parentArr.[i] then
+                                newParent.[i] <- root
+                                changed <- true
+                        if changed then pointerJump newParent (maxIter - 1) else newParent
+
+                let parent''' = pointerJump parentCopy (int n)
+
+                let s2 (i: uint64<Matrix.rowindex>) (j: uint64<Matrix.colindex>) (_: 'w) =
+                    let ii = uint64 i
+                    let jj = uint64 j
+                    findRoot parent''' ii <> findRoot parent''' jj
+
+                let S' =
+                    Matrix.toCoordinateList S
+                    |> fun cl ->
+                        let filtered = cl.list |> List.filter (fun (i, j, w) -> s2 i j w)
+                        Matrix.fromCoordinateList (Matrix.CoordinateList(cl.nrows, cl.ncols, filtered))
+
+                inner S' newMstEdges parent''' rankCopy (iterations + 1)
+
+        else
+            let resultCL =
+                Matrix.CoordinateList(graph.nrows, graph.ncols,
+                    List.map (fun (i, j, w) -> (i * 1UL<Matrix.rowindex>, j * 1UL<Matrix.colindex>, w)) mstEdges)
+            Result.Success (Matrix.fromCoordinateList resultCL)
+
+    let parentInit = [| for i in 0UL..n-1UL -> i |]
+    let rankInit = [| for i in 0UL..n-1UL -> 0UL |]
+    inner graph [] parentInit rankInit 0
