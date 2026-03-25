47124d33bb58572f549ba7f7710758b435734a99
gsv <rsdpisuy@gmail.com>
2026-03-25 13:02:40 +0300

    First part of Borivka works.

--- QuadTree.Tests/Tests.Boruvka.fs
+++ QuadTree.Tests/Tests.Boruvka.fs
@@ -8,6 +8,163 @@ open Vector
 open Common
 
 
+//[<Fact>]
+let ``Boruvka MST simple triangle.`` () =
+    System.Console.Error.WriteLine("TEST STARTING")
+    let graph =
+        let clist =
+            Matrix.CoordinateList(7UL<nrows>, 7UL<ncols>,[
+                0UL<rowindex>, 1UL<colindex>, 1UL
+                1UL<rowindex>, 0UL<colindex>, 1UL
+
+                0UL<rowindex>, 2UL<colindex>, 1UL 
+                2UL<rowindex>, 0UL<colindex>, 1UL 
+
+                1UL<rowindex>, 2UL<colindex>, 1UL 
+                2UL<rowindex>, 1UL<colindex>, 1UL 
+
+                ])
+
+        Matrix.fromCoordinateList clist
+
+    
+    let expected =
+        let clist =
+            Matrix.CoordinateList(7UL<nrows>, 7UL<ncols>,[
+                0UL<rowindex>, 1UL<colindex>, 1UL
+                1UL<rowindex>, 0UL<colindex>, 1UL
+
+                0UL<rowindex>, 2UL<colindex>, 1UL 
+                2UL<rowindex>, 0UL<colindex>, 1UL 
+                ])
+
+        Matrix.fromCoordinateList clist
+        |> Result.Success
+
+    //let actual = 
+    match Graph.Boruvka.mst graph with 
+    | Result.Success tree -> 
+        let tree_transposed = Matrix.transpose tree
+        let actual = Matrix.map2 tree tree_transposed (fun x y -> match (x,y) with | (Some(x),_) | (_, Some x) -> Some x | _ -> None)
+        match actual with 
+        | Result.Success actual -> Tests.printMatrixCoordinate actual
+        | _ -> printfn "Failed"
+        Assert.Equal(expected, actual)
+        //actual
+        //|> Result.Success
+    | x -> Assert.Fail (sprintf "Boruvka failed: %A" x)
+
+
+//[<Fact>]
+let ``Boruvka MST simple square.`` () =
+    System.Console.Error.WriteLine("TEST STARTING")
+    let graph =
+        let clist =
+            Matrix.CoordinateList(7UL<nrows>, 7UL<ncols>,[
+                0UL<rowindex>, 1UL<colindex>, 1UL
+                1UL<rowindex>, 0UL<colindex>, 1UL
+
+                1UL<rowindex>, 2UL<colindex>, 1UL 
+                2UL<rowindex>, 1UL<colindex>, 1UL 
+
+                2UL<rowindex>, 3UL<colindex>, 1UL 
+                3UL<rowindex>, 2UL<colindex>, 1UL 
+
+                0UL<rowindex>, 3UL<colindex>, 1UL 
+                3UL<rowindex>, 0UL<colindex>, 1UL 
+
+                ])
+
+        Matrix.fromCoordinateList clist
+
+    
+    let expected =
+        let clist =
+            Matrix.CoordinateList(7UL<nrows>, 7UL<ncols>,[
+                0UL<rowindex>, 1UL<colindex>, 1UL
+                1UL<rowindex>, 0UL<colindex>, 1UL
+
+                0UL<rowindex>, 3UL<colindex>, 1UL 
+                3UL<rowindex>, 0UL<colindex>, 1UL 
+
+                1UL<rowindex>, 2UL<colindex>, 1UL 
+                2UL<rowindex>, 1UL<colindex>, 1UL 
+                ])
+
+        Matrix.fromCoordinateList clist
+        |> Result.Success
+
+    //let actual = 
+    match Graph.Boruvka.mst graph with 
+    | Result.Success tree -> 
+        let tree_transposed = Matrix.transpose tree
+        let actual = Matrix.map2 tree tree_transposed (fun x y -> match (x,y) with | (Some(x),_) | (_, Some x) -> Some x | _ -> None)
+        match actual with 
+        | Result.Success actual -> Tests.printMatrixCoordinate actual
+        | _ -> printfn "Failed"
+        Assert.Equal(expected, actual)
+        //actual
+        //|> Result.Success
+    | x -> Assert.Fail (sprintf "Boruvka failed: %A" x)
+
+
+
+
+//[<Fact>]
+let ``Boruvka MST simple square in two steps.`` () =
+    System.Console.Error.WriteLine("TEST STARTING")
+    let graph =
+        let clist =
+            Matrix.CoordinateList(7UL<nrows>, 7UL<ncols>,[
+                0UL<rowindex>, 1UL<colindex>, 2UL
+                1UL<rowindex>, 0UL<colindex>, 2UL
+
+                1UL<rowindex>, 2UL<colindex>, 1UL 
+                2UL<rowindex>, 1UL<colindex>, 1UL 
+
+                2UL<rowindex>, 3UL<colindex>, 2UL 
+                3UL<rowindex>, 2UL<colindex>, 2UL 
+
+                0UL<rowindex>, 3UL<colindex>, 1UL 
+                3UL<rowindex>, 0UL<colindex>, 1UL 
+
+                ])
+
+        Matrix.fromCoordinateList clist
+
+    
+    let expected =
+        let clist =
+            Matrix.CoordinateList(7UL<nrows>, 7UL<ncols>,[
+                0UL<rowindex>, 1UL<colindex>, 2UL
+                1UL<rowindex>, 0UL<colindex>, 2UL
+
+                0UL<rowindex>, 3UL<colindex>, 1UL 
+                3UL<rowindex>, 0UL<colindex>, 1UL 
+
+                1UL<rowindex>, 2UL<colindex>, 1UL 
+                2UL<rowindex>, 1UL<colindex>, 1UL 
+                ])
+
+        Matrix.fromCoordinateList clist
+        |> Result.Success
+
+    //let actual = 
+    match Graph.Boruvka.mst graph with 
+    | Result.Success tree -> 
+        let tree_transposed = Matrix.transpose tree
+        let actual = Matrix.map2 tree tree_transposed (fun x y -> match (x,y) with | (Some(x),_) | (_, Some x) -> Some x | _ -> None)
+        match actual with 
+        | Result.Success actual -> Tests.printMatrixCoordinate actual
+        | _ -> printfn "Failed"
+        Assert.Equal(expected, actual)
+        //actual
+        //|> Result.Success
+    | x -> Assert.Fail (sprintf "Boruvka failed: %A" x)
+
+
+
+
 [<Fact>]
 let ``Boruvka MST.`` () =
     System.Console.Error.WriteLine("TEST STARTING")
@@ -82,9 +239,10 @@ let ``Boruvka MST.`` () =
     | Result.Success tree -> 
         let tree_transposed = Matrix.transpose tree
         let actual = Matrix.map2 tree tree_transposed (fun x y -> match (x,y) with | (Some(x),_) | (_, Some x) -> Some x | _ -> None)
+        match actual with 
+        | Result.Success actual -> Tests.printMatrixCoordinate actual
+        | _ -> printfn "Failed"
         Assert.Equal(expected, actual)
-        //actual
-        //|> Result.Success
     | x -> Assert.Fail (sprintf "Boruvka failed: %A" x)
 
     
