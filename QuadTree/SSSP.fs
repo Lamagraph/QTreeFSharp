dff1cac06dcb83f8f48d6040b3d8c275e24fc7d8
gsv <rsdpisuy@gmail.com>
2026-03-27 17:29:01 +0300

    Apply result workflow for SSSP and TriangleCount.

--- QuadTree/SSSP.fs
+++ QuadTree/SSSP.fs
@@ -7,6 +7,10 @@ type Error =
     | FrontierCalculationProblem of Vector.Error
     | VisitedCalculationProblem of Vector.Error
 
+let mapError (err: LinearAlgebra.Error) = NewFrontierCalculationProblem err
+let mapError' (err: Vector.Error) = FrontierCalculationProblem err
+let mapError'' (err: Vector.Error) = VisitedCalculationProblem err
+
 let sssp graph (startVertex: uint64) =
     let op_add x y =
         match (x, y) with
@@ -22,29 +26,27 @@ let sssp graph (startVertex: uint64) =
 
     let rec inner (frontier: Vector.SparseVector<_>) (visited: Vector.SparseVector<_>) iter_num =
         if frontier.nvals > 0UL<nvals> && iter_num <= int frontier.length then
+            result {
+                let! new_frontier =
+                    LinearAlgebra.vxm op_add op_mult frontier graph
+                    |> Common.Result.mapError mapError
 
-            let new_frontier = LinearAlgebra.vxm op_add op_mult frontier graph
-
-            match new_frontier with
-            | Error(e) -> Error(NewFrontierCalculationProblem(e))
-            | Ok(new_frontier) ->
                 let op_min x y =
                     match (x, y) with
                     | Some v, Some u -> if v < u then Some v else None
                     | Some v, _ -> Some v
                     | _ -> None
 
-                let frontier = Vector.map2 new_frontier visited op_min
+                let! frontier =
+                    Vector.map2 new_frontier visited op_min
+                    |> Common.Result.mapError mapError'
 
-                match frontier with
-                | Error(e) -> Error(FrontierCalculationProblem(e))
-                | Ok(frontier) ->
+                let! visited =
+                    Vector.map2 visited frontier op_add
+                    |> Common.Result.mapError mapError''
 
-                    let visited = Vector.map2 visited frontier op_add
-
-                    match visited with
-                    | Error(e) -> Error(VisitedCalculationProblem(e))
-                    | Ok(visited) -> inner frontier visited (iter_num + 1)
+                return! inner frontier visited (iter_num + 1)
+            }
         else
             Ok visited
 
